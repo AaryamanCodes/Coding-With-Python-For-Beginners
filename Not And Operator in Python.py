30193e940bdc83899de5c4b9933fd6b02cc0e77c
AaryamanCodes <60293836+AaryamanCodes@users.noreply.github.com>
2020-07-29 16:26:36 +0530

    Update Not And Operator in Python.py

--- Not And Operator in Python.py
+++ Not And Operator in Python.py
@@ -1,3 +1,10 @@
+"""
+This is the NAND operator in Python. The NAND operator dosen't exist, so I made my own NAND operator.
+This works simply on the logic of the NAND gate which means that it will be the opposite result of 
+what and would give thus given the abbreviation of NAND which means Not And.
+"""
+
+#Defining the Function
 def nand(n1, n2):
     res = n1 and n2
     notRes = not res
